09f8d8577c67beb4ed8cf3b5b24f21c40eb7e479
João Santos <joaomarcelo.ms@gmail.com>
2024-08-16 08:38:27 -0300

    ajuste mock json

--- app/src/main/java/com/jomar/poc/mygeofenceeapp/JSONmockList.kt
+++ app/src/main/java/com/jomar/poc/mygeofenceeapp/JSONmockList.kt
@@ -1,5 +1,10 @@
 package com.jomar.poc.mygeofenceeapp
 
+import com.jomar.poc.mygeofenceeapp.model.GeofenceModel.Companion.CASA_CAROL
+import com.jomar.poc.mygeofenceeapp.model.GeofenceModel.Companion.DR_IVAN_DENTISTA
+import com.jomar.poc.mygeofenceeapp.model.GeofenceModel.Companion.ESCOLA
+import com.jomar.poc.mygeofenceeapp.model.GeofenceModel.Companion.MINHA_CASA
+import com.jomar.poc.mygeofenceeapp.model.GeofenceModel.Companion.VERDE_MAR
 import org.json.JSONArray
 import org.json.JSONObject
 const val ID = "id"
@@ -9,41 +14,45 @@ const val RADIUS = "radius"
 
 fun getJsonMockList() : String {
     val home =  JSONObject()
-    home.put(ID,"HOME")
-    home.put(LAT,-19.9301908)
-    home.put(LONG,-43.9666872)
-    home.put(RADIUS,100.0)
-
     val carol =  JSONObject()
-    carol.put(ID,"CAROL APTO")
-    carol.put(LAT,-19.923648)
-    carol.put(LONG,-43.945055)
-    carol.put(RADIUS,100.0)
-
-    val school =  JSONObject()
-    school.put(ID,"ESCOLA")
-    school.put(LAT,-19.940291)
-    school.put(LONG,-43.961461)
-    school.put(RADIUS,100.0)
-
-    val ivan =  JSONObject()
-    ivan.put(ID,"IVAN DENTISTA")
-    ivan.put(LAT,-19.9298106)
-    ivan.put(LONG,-43.9340675)
-    ivan.put(RADIUS,100.0)
-
     val verdeMar =  JSONObject()
-    verdeMar.put(ID,"VERDEMAR")
-    verdeMar.put(LAT,-19.926942)
-    verdeMar.put(LONG,-43.964864)
-    verdeMar.put(RADIUS,100.0)
+    val ivan =  JSONObject()
+    val school =  JSONObject()
+
+    home.apply {
+        put(ID,MINHA_CASA)
+        put(LAT,MINHA_CASA.latitude)
+        put(LONG,MINHA_CASA.longitude)
+        put(RADIUS,MINHA_CASA.radius)
+    }
+    carol.apply {
+        put(ID,CASA_CAROL)
+        put(LAT,CASA_CAROL.latitude)
+        put(LONG,CASA_CAROL.longitude)
+        put(RADIUS,CASA_CAROL.radius)
+    }
+    verdeMar.apply {
+        put(ID,VERDE_MAR)
+        put(LAT,VERDE_MAR.latitude)
+        put(LONG,VERDE_MAR.longitude)
+        put(RADIUS,VERDE_MAR.radius)
+    }
+    ivan.apply {
+        put(ID,DR_IVAN_DENTISTA)
+        put(LAT,DR_IVAN_DENTISTA.latitude)
+        put(LONG,DR_IVAN_DENTISTA.longitude)
+        put(RADIUS,DR_IVAN_DENTISTA.radius)
+    }
+    school.apply {
+        put(ID,ESCOLA)
+        put(LAT,ESCOLA.latitude)
+        put(LONG,ESCOLA.longitude)
+        put(RADIUS,ESCOLA.radius)
+    }
 
     val resultList = listOf(home,carol,school,ivan,verdeMar)
-
     val json = JSONArray()
-    for (item in resultList) {
-        json.put(item)
-    }
+    for (item in resultList) { json.put(item) }
 
     return json.toString()
 }
